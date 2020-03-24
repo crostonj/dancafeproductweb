5b88c9ef3f3913243f9ac7634dec1e7bdb72fdd1
Jeff Croston <crostonj@gmail.com>
2020-03-23 18:47:18 -0600

    Creating custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,36 @@
+
 import { ProductHomeModule } from './ProductHome/ProductHome.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { ProductListComponent } from './ProductHome/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailsComponent } from './ProductDetails/product-details.component';
 import { ProductSearchComponent } from './ProductHome/product-search/product-search.component';
-import { AppRoutingModule } from './app.routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { Router } from '@angular/router';
-
-
+declare const createCustomElement;
 
 
 @NgModule({
    declarations: [
-      AppComponent,
       ProductDetailsComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
-      AppRoutingModule,
       ProductHomeModule
    ],
    providers: [],
-   bootstrap: [
-      AppComponent,
-      PageNotFoundComponent
+   entryComponents: [
+     ProductDetailsComponent,
+     ProductListComponent,
+     ProductSearchComponent,
+     ProductHomeModule
    ]
 })
-export class AppModule {
+export class ProductModule {
    // Diagnostic only: inspect router configuration
   constructor(router: Router) {
+    const productDetailsView = createCustomElement(ProductDetailsComponent);
 
   }
 }
